a0b97b6f1978462928e7b616d9528a5a26e2d288
Brayan Canul Tamay <a.brayan.canul@utponiente.edu.mx>
2023-02-04 12:18:13 -0600

    avance 1.1

---
+++ Avance 1.1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Bienvenido</title>
+    <link rel="stylesheet" href="Recursos/CSS/Estilo.css">
+</head>
+<body>
+    <header>
+        <a href="/Login_PHP">MyPageWeb</a>
+    </header>
+
+    <h1>Inicia sesión o registrate</h1>
+
+    <a href= "login.php" class="acceso">Iniciar sesión</a><br><br>
+    <a href= "registrarse.php" class="acceso">Registrarse</a>
+</body>
+</html>
